8e79bb0311c0dae3c1b3ee566556dd3db49a1d65
adityarizky1C <114641440+adityarizky1C@users.noreply.github.com>
2022-10-06 11:43:18 +0700

    TUGAS 1 DAN 2

---
+++ Tugas 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<" NPM : 2210631170109 "<< endl;
+    cout<<" Nama lengkap : Aditya Rizky Darmawan "<< endl;
+    cout<<" Umur : 18 Tahun "<< endl;
+    cout<<" Kelas : 1C Informatika "<< endl;
+    cout<<" Hobi : Menyanyi "<< endl;
+    cout<<" Cita - Cita : Ingin Hidup Dengan Tentram"<< endl;
+
+    return 0;
+}
